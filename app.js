a68d5fd2340df6e2e1a2db5caf8b727181e8215d
unknown <senac@uept46-13597-a.aluno.pr.senac.br>
2022-08-25 20:41:32 -0300

    anexando os arquivos app.js index.html stle.css

---
+++ app.js
@@ -0,0 +1,19 @@
+let a =1
+let b =2*a
+
+function requestOfServer() {
+
+    $.ajax({
+        type: 'get',
+        url: "http://localhost:8000/hello-world?info=OlaMundo"
+    }).done(function(data) { 
+        document.getElementById('developer').innerHTML = data.name
+        document.getElementById('version').innerHTML = data.version
+        document.getElementById('value-of-variable').innerHTML = data.value_of_variable_info
+        document.getElementById('company-site').innerHTML = data.web_site_company
+
+        $("#show-values").css('display', 'block')
+
+    });
+
+}
